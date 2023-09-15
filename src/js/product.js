06a4baa967bb6af1c50eb2d63959f8f20e527900
Joshua Pearson <jpearson@ximasoftware.com>
2023-09-15 13:47:50 -0600

    cart works as intended

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,12 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
+const getCart = () => getLocalStorage("so-cart") || [];
+
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  setLocalStorage("so-cart", [...getCart, product]);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
